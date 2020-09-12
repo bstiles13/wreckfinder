81c41e51995b25f1f41572346e2b5d07f67f9220
Brandon Stiles <bstiles@merkleinc.com>
2020-09-12 12:31:15 -0400

    adds redux

--- src/index.js
+++ src/index.js
@@ -5,7 +5,25 @@ import App from './App';
 import 'semantic-ui-css/semantic.min.css';
 import './index.scss';
 
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import reducers from './store/reducers';
+
+let middleware = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  const logger = createLogger({ collapsed: true });
+  middleware = [...middleware, logger];
+}
+
+const store = createStore(reducers, applyMiddleware(...middleware));
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>
+  ,
   document.getElementById('root')
 );
