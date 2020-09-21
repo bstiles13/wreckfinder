c96799b5e7873ee4146142b74e025b50e592709d
Brandon Stiles <bstiles@merkleinc.com>
2020-09-20 22:30:05 -0400

    adds quick search message

--- src/components/Sidebar/Search/BasicSearch/BasicSearch.js
+++ src/components/Sidebar/Search/BasicSearch/BasicSearch.js
@@ -1,20 +1,29 @@
 import React from 'react';
-import { Form } from 'semantic-ui-react';
+import { Form, Message } from 'semantic-ui-react';
 
 // import './Search.scss';
 
 export const BasicSearch = ({ description, handleChange }) => {
   return (
-    <Form.Input
-      id='description'
-      fluid
-      label='Keyword(s)'
-      placeholder='Sunk by submarine'
-      icon='ship'
-      iconPosition='left'
-      onChange={handleChange}
-      value={description}
-      autoFocus
-    />
+    <>
+      <Form.Input
+        id='description'
+        fluid
+        label='Keyword(s)'
+        placeholder='Sunk by submarine'
+        icon='ship'
+        iconPosition='left'
+        onChange={handleChange}
+        value={description}
+        autoFocus
+      />
+      <Message
+        info
+        size='tiny'
+        icon='pointing right'
+        header='Quick Search'
+        content={`Find wrecks in United States and territorial waters with names and descriptions matching your text`}
+      />
+    </>
   );
 };
