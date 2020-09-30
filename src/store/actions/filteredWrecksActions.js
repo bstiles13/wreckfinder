6bd2cb024acc3f6b5281b428a765bfa04ec8c698
Brandon Stiles <bstiles@merkleinc.com>
2020-09-30 14:07:32 -0400

    focuses wreck when single result in search

--- src/store/actions/filteredWrecksActions.js
+++ src/store/actions/filteredWrecksActions.js
@@ -1,8 +1,14 @@
+import { setSelectedWreck } from './index';
 export const SET_FILTERED_WRECKS = 'SET_FILTERED_WRECKS';
 
-export const setFilteredWrecks = (filteredWrecks) => {
-  return {
+export const setFilteredWrecks = (filteredWrecks) => (dispatch) => {
+  dispatch({
     type: SET_FILTERED_WRECKS,
     payload: filteredWrecks
-  };
+  });
+
+  // If a single result then focus wreck
+  if (filteredWrecks && filteredWrecks.length === 1) {
+    dispatch(setSelectedWreck({ ...filteredWrecks[0], focus: true }));
+  }
 };
