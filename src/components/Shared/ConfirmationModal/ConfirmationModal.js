1ba818ce148cef1d532edeefd5d690dea9438181
Brandon Stiles <bstiles@merkleinc.com>
2020-10-04 15:32:55 -0400

    adds ConfirmationModal shared component

---
+++ src/components/Shared/ConfirmationModal/ConfirmationModal.js
@@ -0,0 +1,41 @@
+import React, { Fragment } from 'react';
+import { Button, Modal } from 'semantic-ui-react';
+
+import './ConfirmationModal.scss';
+
+export const ConfirmationModal = ({
+  children,
+  className,
+  header,
+  onConfirm,
+  onCancel,
+  modalOpen,
+  size = 'small'
+}) => (
+  <Fragment>
+    <Modal
+      className={`confirmation-modal ${className}`}
+      open={modalOpen}
+      onClose={onCancel}
+      centered={false}
+      size={size}
+    >
+      <Modal.Header className='confirmation-modal-header'>{header || 'Confirmation'}</Modal.Header>
+      <Modal.Content className='confirmation-modal-content'>{children}</Modal.Content>
+      <Modal.Actions>
+        <Button
+          className='m1-button-secondary confirmation-modal-cancel-button'
+          onClick={onCancel}
+        >
+          Cancel
+        </Button>
+        <Button
+          className='button-primary confirmation-modal-proceed-button'
+          onClick={onConfirm}
+        >
+          Proceed
+        </Button>
+      </Modal.Actions>
+    </Modal>
+  </Fragment>
+);
